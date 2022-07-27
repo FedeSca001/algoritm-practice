6618da115a89b0e4a098c3a69e9d73ad749f1984
FedeSca001 <familadore@gmail.com>
2022-07-27 19:20:24 +0200

    practice

--- Algoritmos/number2/index.js
+++ Algoritmos/number2/index.js
@@ -1,11 +1,17 @@
-let n = 0;
+/*
+Desarrolle  un algoritmo  que realice  la sumatoria  de los números enteros  múltiplos  de 5,
+comprendidos entre  el  1  y el   100,  es decir,  5  + 10  +  15  +….  +  100. 
+El  programa  deberá  imprimir  los  números  en cuestión y finalmente su sumatoria
+*/
+
+let n=0;
 let plus = 0;
 function iteration(){
-    for (let i = 1; i < 11; i++) {
-        n = n+1;
+    for (let i = 0; n < 95; i++) {
+        n = n+5;
         plus = plus + n;
-        console.log(n);
+        console.log('Number:'+n);
     }
-    console.log(plus);
+    console.log('Result: '+plus);
 }
 iteration();
---
+++ volumen/circle/index.js
@@ -0,0 +1,9 @@
+const r = 2;
+const h = 6;
+
+function circleArea(){
+    const volumen = Math.PI * h * Math.pow(r, 2);
+    console.log('El volumen del cilindro es '+volumen);
+}
+
+circleArea();
