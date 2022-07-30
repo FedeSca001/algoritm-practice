5c574a4d64a4bc1e4800be00bd53a3ad82d58f55
FedeSca001 <familadore@gmail.com>
2022-07-30 11:50:47 +0200

    logic

--- conjetura de Collatz/index.js
+++ conjetura de Collatz/index.js
@@ -1,10 +1,10 @@
-const input = 3
+const input = 231
 const results = [input];
 //La conjetura de Collatz
 //Si el número es par se divide en 2.
 //Si el número es impar se multiplica por 3 y al resultado se le suma 1.
 function operation (){
-    for(let i = 0; i<50; i++){
+    for(let i = 0; i<60; i++){
         let number = Number(results[results.length - 1]);
         if (number%2 ===0){
             results.push(number/2);
