aeea5c947a6c94ac8f3bc9fea8030a1bf658b06b
FedeSca001 <familadore@gmail.com>
2022-08-18 12:54:04 +0200

    number 5

--- Algoritmos/number5/index.js
+++ Algoritmos/number5/index.js
@@ -9,20 +9,3 @@
  * - Cada elemento se repite dos años seguidos.
  * - El último ciclo sexagenario comenzó en 1984 (Madera Rata).
  */
-
-const elementos = ['madera', 'fuego', 'tierra', 'metal', 'agua'];
-const animales = ['rata', 'buey', 'tigre', 'conejo', 'dragón', 'serpiente', 'caballo', 'oveja', 'mono', 'gallo', 'perro', 'cerdo'];
-const ciclos = [];
-
-function cicloSexagenario(el, an){
-    for (let i = 0; i <= 60; i++){
-        const iterElemento = 0;
-        ciclos.push({
-                elemento: el[iterElemento],
-                animal: an[i],
-        });
-        console.log(iterElemento);
-        iterElemento > 5 ? iterElemento = 0 : iterElemento++;
-    }
-}
-cicloSexagenario(elementos, animales);
