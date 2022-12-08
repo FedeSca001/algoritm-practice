f12833de358a5db6431a114b646a9d71d1ad8332
FedeSca001 <familadore@gmail.com>
2022-12-08 14:48:10 +0100

    es dificil

--- Algoritmos/dia5/index.js
+++ Algoritmos/dia5/index.js
@@ -1,16 +1,34 @@
 /*
-Para no cansar a los renos, Papá Noel quiere dejar el máximo número de regalos haciendo el menor número posible de viajes.
+Para no cansar a los renos, Papá Noel quiere dejar el máximo número de regalos 
+haciendo el menor número posible de viajes.
 
-Tiene un array de ciudades donde cada elemento es el número de regalos que puede dejar allí. [12, 3, 11, 5, 7]. Por otro lado, el límite de regalos que caben en su saco. Y, finalmente, el número de ciudades máximo que sus renos pueden visitar.
+Tiene un array de ciudades donde cada elemento es el número de regalos que puede dejar allí. 
+[12, 3, 11, 5, 7]. 
+Por otro lado, el límite de regalos que caben en su saco. 
+Y, finalmente, el número de ciudades máximo que sus renos pueden visitar.
 
-Como no quiere dejar una ciudad a medias, si no puede dejar todos los regalos que son de esa ciudad, no deja ninguno allí.
+Como no quiere dejar una ciudad a medias, si no puede dejar todos los 
+regalos que son de esa ciudad, no deja ninguno allí.
 
-Crea un programa que le diga la suma más alta de regalos que podría repartir teniendo en cuenta el máximo de regalos que puede transportar y el número máximo de ciudades que puede visitar:
+Crea un programa que le diga la suma más alta de regalos 
+que podría repartir teniendo en cuenta el máximo de regalos que puede transportar 
+y el número máximo de ciudades que puede visitar:
 */
-const giftsCities = [12, 3, 11, 5, 7]
-const maxGifts = 20
-const maxCities = 3
+const giftsCities = [12, 3, 11, 5, 7];
+const maxGifts = 20;
+const maxCities = 3;
 
+function getMaxGifts(giftsCities, maxGifts, maxCities) {
+        if (maxGifts >= 1 && giftsCities.length >= 1 && maxCities >= 1) {
+                if (Math.max(...giftsCities) < maxGifts) {
+                        console.log('puedo');
+                }
+                let sumaRegalos = 0;
+                return sumaRegalos;
+        } else {
+                return 0;
+        }
+}
 // la suma más alta de regalos a repartir
 // visitando un máximo de 3 ciudades
 // es de 20: [12, 3, 5]
@@ -19,7 +37,7 @@ const maxCities = 3
 // pero excede el límite de 20 regalos y tendría
 // que dejar alguna ciudad a medias.
 
-getMaxGifts(giftsCities, maxGifts, maxCities) // 20
+getMaxGifts(giftsCities, maxGifts, maxCities); // 20
 /*
 Si no se puede realizar ningún viaje que satisface los requerimientos, el resultado debe ser 0. Más ejemplos:
 getMaxGifts([12, 3, 11, 5, 7], 20, 3) // 20
