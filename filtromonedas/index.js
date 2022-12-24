8980f69da671360df2b4d6e22c0d75d6e89be186
FedeSca001 <familadore@gmail.com>
2022-12-24 19:57:18 +0100

    con el cubano

---
+++ filtromonedas/index.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const rl = readline.createInterface({ input: process.stdin });
+/*process.stdin.on("data", (c) => {
+
+    console.log(String(c),String(b));
+});
+*/
+
+
+
+let n = false;
+rl.on("line", (w) => {
+        if (n == false) {
+                n = true;
+                return;
+        }
+
+        if (w.length > 10) {
+                w =
+                        w.substr(0, 1) +
+                        String(w.length - 2) +
+                        w.substr(w.length - 1, 1);
+        }
+        console.log(w);
+});
