4d8ff46a0b6d62ad08cf8242cd39b702caf5fdb2
FedeSca001 <familadore@gmail.com>
2023-01-03 22:40:48 +0100

    funcinoa

--- filtromonedas/index.js
+++ filtromonedas/index.js
@@ -35,20 +35,26 @@ rl.on("line", (w) => {
         }
         contenedorMonedas.push(Number(w));
         if (contenedorMonedas.length == n) {
-                let contenedorPequeño = [0];
-                contenedorMonedas.sort(function (a, b) {return a - b;}).reverse() //ordeno el array de mas a menos
-                .forEach(e => {
-                        //Hacer un foreach() de contenedor monedas \\ dentro el if
-                        if ((contenedorPequeño[contenedorPequeño.length - 1] += e) > 4) {
-                                console.log('hacer push a contpequeño -- sumar elemnto mas contpequeño[ultimo]');
-                                contenedorPequeño.push(0)
-                                contenedorPequeño[contenedorPequeño.length - 1] += e;
-                        } else {
-                                console.log('sumar elemnto mas contpequeño[ultimo]');
-                                contenedorPequeño[contenedorPequeño.length - 1] += e;
+                let referenciaIndex = new Array(n).fill(null);
+                contenedorMonedas.sort(function (a, b) {return b - a;}) //ordeno el array de mas a menos
+                let contadorSaco = 0;//cantidad de sacos que se encuentran en el algoritmo
+                let contadorSuma = 0;//lleva la suma de la iteracion (si llega a 5 pasa a otro saco)
+                let cantNoAsignadas = n;
+                while (cantNoAsignadas > 0) {
+                        for (let index = 0; index < contenedorMonedas.length; index++) {
+                                let element = contenedorMonedas[index];
+                                if (referenciaIndex[index] === null) {
+                                      if ((contadorSuma + element) <= 5) {
+                                              referenciaIndex[index] = contadorSaco;
+                                              contadorSuma+=element;
+                                              cantNoAsignadas -= 1;
+                                      }
+                                }
                         }
-                        contenedorMonedas.shift();//elimina el elemento que se envio al contpequeño       
-                });
-                console.log('log final',contenedorPequeño);
+                        contadorSaco += 1;
+                        contadorSuma = 0;
+                }                
+                console.log('Contenedor inicial', contenedorMonedas);
+                console.log('ReferenciaIndex', referenciaIndex);
         }
 });
