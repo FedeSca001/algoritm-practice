fd9ea07c5b05a3f7aedb5ece400a8eaad60e48dd
FedeSca001 <familadore@gmail.com>
2023-01-04 20:53:57 +0100

    add problem

--- tranviaCoderForce/index.js
+++ tranviaCoderForce/index.js
@@ -1,4 +1,19 @@
-const readline = require("readline");
+process.stdin.on('data', input => {
+  const stops = String(input)
+    .split('\r\n')
+    .slice(1, -1)
+    .map(line => 
+      line.split(' ').map(Number)
+    )
+  let [max, current] = [0, 0]
+  stops.forEach(stop => {
+    current += stop[1] - stop[0]
+    max = Math.max(current, max)
+  })
+  console.log(max)
+})
+
+/*const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin });
 
 let n = false;
@@ -7,4 +22,4 @@ rl.on("line", (w) => {
                 n = Number(w);
                 return;
         }
-});
+});*/
