4c4ac0d6eb161dbf32000e78e787cce524317a9d
FedeSca001 <familadore@gmail.com>
2023-01-04 21:04:50 +0100

    listo para programar

--- tranviaCoderForce/index.js
+++ tranviaCoderForce/index.js
@@ -1,25 +1,16 @@
-process.stdin.on('data', input => {
-  const stops = String(input)
-    .split('\r\n')
-    .slice(1, -1)
-    .map(line => 
-      line.split(' ').map(Number)
-    )
-  let [max, current] = [0, 0]
-  stops.forEach(stop => {
-    current += stop[1] - stop[0]
-    max = Math.max(current, max)
-  })
-  console.log(max)
-})
-
-/*const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin });
-
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+ 
+const input = [];
 let n = false;
-rl.on("line", (w) => {
-        if (n == false) {
-                n = Number(w);
-                return;
+readLine.on('line', 
+  (w) => {
+          if (n == false) {
+                  n = Number(w);
+                  return;
+          }
+          console.log(w)
         }
-});*/
+  );
