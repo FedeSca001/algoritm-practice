8ef3e022adeb66c05ab47115f4263cd22271c80d
FedeSca001 <familadore@gmail.com>
2023-01-08 10:39:54 +0100

    add new

---
+++ proximaRonda/index.js
@@ -0,0 +1,22 @@
+const readLine = require('readline').createInterface(
+    {input: process.stdin,output: process.stdout,}
+);
+const input = [];
+let n = 2;
+readLine.on('line',
+    (w) => {
+        input.push(w)
+        if (input.length == n){
+            let array1 = input[0].split(' ');
+            let array2 = input[1].split(' ');
+            let contadorParticipantes = 0;
+            for (let i = 0; i < array2.length; i++){
+                const element = Number(array2[i]);
+                if (element >= Number(array2[Number(array1[1])]) && element !== 0 ){
+                    contadorParticipantes += 1;
+                }
+            }
+            console.log(contadorParticipantes);
+        }
+    }
+  );
