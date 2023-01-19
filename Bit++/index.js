f26294925cb5e79d14a947b46763e218d4a734e0
FedeSca001 <familadore@gmail.com>
2023-01-19 17:41:03 +0100

    otro mas

--- Bit++/index.js
+++ Bit++/index.js
@@ -1,2 +1,22 @@
 //https://codeforces.com/problemset/problem/282/A
-console.log('hola');
+
+const readLine = require('readline').createInterface({input: process.stdin,output: process.stdout,});
+const input = [];
+let n = false;
+let x = 0;
+readLine.on('line',
+        (w) => {
+            if (n == false){
+                n = Number(w);
+                return;
+            }
+            input.push(w)
+            if (input.length === n) {
+                for (let i = 0; i < input.length; i++) {
+                    const ele = input[i];
+                    ele[1] === '+' ? x++ : x--;
+                }
+                console.log(x);
+            }
+        }
+  );
