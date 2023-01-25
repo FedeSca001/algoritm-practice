29580ea2b18efee16371b4a70ceb494ff13a79ae
FedeSca001 <familadore@gmail.com>
2023-01-25 23:16:41 +0100

    new

---
+++ camisetasDeFootball/index.js
@@ -0,0 +1,17 @@
+// https://codeforces.com/problemset/problem/268/A
+
+const readLine = require('readline').createInterface({input: process.stdin,output: process.stdout,});
+const input = [];
+let n = false;
+readLine.on('line',
+        (w) => {if (n == false){n = Number(w);return;}//n es el valor del caracter ingresado
+                input.push(w)//Agrega el valor ingresado al array de input
+                if (input.length == n) {
+                        let numer = 0;
+                        let array = [0,1,2]
+                        let string = 'Cadena'
+                    console.log(numer,array,string)
+                    process.exit();
+                }
+        }
+  );
--- detectUser/index.js
+++ detectUser/index.js
@@ -2,13 +2,32 @@
 // Eficiencia en el
 // codigo https://medium.com/@juanelojga/algoritmos-en-javascript-parte-1-47a0b13db35a
 // https://www.freecodecamp.org/espanol/news/la-complejidad-de-los-algoritmos-simples-y-las-estructuras-de-datos-en-js/
+/*function pow(base, exponente){
+    if(exponente == 1) return base
+    if(exponente%2 == 0){
+        res = pow(base, exponente/2)
+        return res*res
+    }else{
+        res = pow(base,exponente-1)
+        return res*base
+    }
+}*/
+
 const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin });
 
 rl.on("line", (w) => {
-    let contadorLetras = 0
-    while (contadorLetras < w.length) {
-        contadorLetras++
+    let contadorLetras = 1;
+    w = w.split('').sort();
+    for (let i = 1; i < w.length; i++) {
+        const element = w[i];
+        if (element !== w[i-1]) {
+            contadorLetras++
+        }
     }
-    console.log(w.length+' '+contadorLetras);
+    if (contadorLetras % 2 === 0) {
+        console.log('CHAT WITH HER!');
+    } else {
+        console.log('IGNORE HIM!');
+    }
 });
