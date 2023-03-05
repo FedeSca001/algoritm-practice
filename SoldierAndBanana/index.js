39e1222d60f5f66360b28f15397f674cf744caa3
FedeSca001 <familadore@gmail.com>
2023-03-05 19:01:29 +0100

    leer etiquetas

--- SoldierAndBanana/index.js
+++ SoldierAndBanana/index.js
@@ -5,9 +5,3 @@ process.stdin.on('data',
         const [k,n,w] = String(input).split(' ').map(Number);
         const costo = k*w*(w+1)/2;
         console.log(Math.max(costo - n, 0));});
-
-/*  process.stdin.on('data', input => {
-  const [k, n, w] = String(input).split(' ').map(Number)
-  const cost = k*w*(w+1)/2
-  console.log(Math.max(cost - n, 0))
-})*/
