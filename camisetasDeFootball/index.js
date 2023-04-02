822b0f93be15efc9f94a94779620833bbf845068
FedeSca001 <familadore@gmail.com>
2023-04-02 11:01:29 +0200

    new

--- camisetasDeFootball/index.js
+++ camisetasDeFootball/index.js
@@ -4,12 +4,14 @@ const readLine = require('readline').createInterface({input: process.stdin,outpu
 const input = [];
 let n = false;
 readLine.on('line',
-        (w) => {if (n == false){n = Number(w);return;}//n es el valor del caracter ingresado
-                input.push(w)//Agrega el valor ingresado al array de input
+        (line) => {if (n == false){n = Number(line);return;}//n es el valor del caracter ingresado
+                input.push(line)//Agrega el valor ingresado al array de input
                 if (input.length == n) {
-                        const [a,b] = input.split(' ');
-                        console.log(a,b);
-                    process.exit();
+                        for (let i = 0; i < input.length; i++) {//Aca dentro va la lógica...
+                                const [a, b] = input[i].split(' ').map(Number);
+                                console.log(a,b);
+                        }
+                        readLine.close();
                 }
         }
   );
