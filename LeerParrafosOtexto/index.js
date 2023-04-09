ced10c48859419efe72b3493b3d371ca3a477559
FedeSca001 <familadore@gmail.com>
2023-04-09 09:57:45 +0200

    leer textos

--- LeerParrafosOtexto/index.js
+++ LeerParrafosOtexto/index.js
@@ -1,14 +1,22 @@
 const readLine = require('readline').createInterface({input: process.stdin,output: process.stdout,});
 let parrafo = [];
+let inputValor = false;
 readLine.on('line',
     (line) =>{
-        parrafo=line.split(' ')//La variable parrafo es un array con todas las palabras separadas
-        /* Como objetivo este código va a retornar la última letra de cada palabra JEJE */
+        if (inputValor === false) {inputValor = line; return}
+        //La variable parrafo es un array con todas las palabras separadas
+        //parrafo=line.split(' ')
+        /* Como objetivo este código va a retornar la última letra de cada palabra JEJE 
         for (let i = 0; i < parrafo.length; i++) {
             const element = parrafo[i];
             console.log(element[element.length-1]);
+        }*/
+        let contadorLetra = 0;
+        for (let i = 0; i < line.length; i++) {
+            const ele = line[i];
+            if (ele === inputValor) contadorLetra ++
         }
-        console.log(parrafo);
-        readLine.close();//Close
+        console.log('La letra ',inputValor,'se repite ',contadorLetra,' veces');
+        //readLine.close();
     }
   );
--- leerEtiquetasWeb/index.js
+++ leerEtiquetasWeb/index.js
@@ -2,18 +2,14 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 // URL de la página web que se va a leer
-const url = 'https://www.ole.com.ar/';
+const url = 'https://www.ole.com.ar/futbol-internacional';
 
-// Hacer una petición HTTP GET a la página web
+
 request(url, (error, response, body) => {
   if (!error && response.statusCode == 200) {
-    // Cargar el HTML en cheerio
     const $ = cheerio.load(body);
-    // Buscar todas las etiquetas h1 en el HTML
-    $('a.title').each((i, el) => {
-      // Imprimir el contenido de cada etiqueta h1 por consola
-      console.log(el,'Index ',i);
-      //console.log(el.attribs.slot,i);
+    $('a').each((i, el) => {
+      console.log(el,i);
     });
   } else {
     console.log('error',error);
