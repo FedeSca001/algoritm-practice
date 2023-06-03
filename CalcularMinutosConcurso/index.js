46739773928d2eba30bb71130347b71ee787036a
FedeSca001 <familadore@gmail.com>
2023-06-03 11:44:59 +0200

    jejeje

--- CalcularMinutosConcurso/index.js
+++ CalcularMinutosConcurso/index.js
@@ -1,16 +1,26 @@
 //https://codeforces.com/problemset/problem/750/A
-const readLine = require('readline').createInterface({input: process.stdin,output: process.stdout,});
- 
-readLine.on('line',
-    (w) => {
-        const [problemas, minutosNecesarios] = w.split(' ')
-        const tiempoDisponible = 240 - Number(minutosNecesarios)
-        let problemasAresolver = 0;
-        let tiempoTotalProblemas = 0;
-        for (let i = 0; i <= Number(problemas); i++) {
-            tiempoTotalProblemas = tiempoTotalProblemas + i*5
-            if (tiempoDisponible >= tiempoTotalProblemas) problemasAresolver = i
-        }
-        console.log(problemasAresolver);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (input) => {
+  const [problemas, minutosNecesarios] = input.split(' ');
+  const tiempoDisponible = 240 - Number(minutosNecesarios);
+  let problemasAresolver = 0;
+  let tiempoTotalProblemas = 0;
+
+  for (let i = 1; i <= Number(problemas); i++) {
+    tiempoTotalProblemas += i * 5;
+    if (tiempoDisponible >= tiempoTotalProblemas) {
+      problemasAresolver = i;
+    } else {
+      break;
     }
-  );
+  }
+
+  console.log(problemasAresolver);
+});
+
