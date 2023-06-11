012a643fe7a08f541c6fbe6ece1c53b2b91592f8
FedeSca001 <familadore@gmail.com>
2023-06-11 12:45:20 +0200

    solution

--- LlegadaDelGeneral/index.js
+++ LlegadaDelGeneral/index.js
@@ -1,17 +1,44 @@
-//https://codeforces.com/problemset/problem/144/A
-const readLine = require('readline').createInterface({input: process.stdin,output: process.stdout,});
- 
-let n = false;
-let contador = 0
-readLine.on('line',
-    (w) => {
-        if (n == false){
-            n = Number(w);
-            return;
-        }
-        const altSoldados = w.split(' ').map(Number)
-        console.log(altSoldados);
-        console.log(contador);
-        readLine.close()
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n = 0;
+let contador = 0;
+let altSoldados = [];
+
+rl.on('line', (input) => {
+  if (n === 0) {
+    n = Number(input);
+  } else {
+    altSoldados = input.split(' ').map(Number);
+
+    let altMaxSoldado = 0;
+    let altMaxSoldadoIndex = -1;
+    let altMinSoldado = 101;
+    let altMinSoldadoIndex = -1;
+
+    for (let i = 0; i < altSoldados.length; i++) {
+      const altura = altSoldados[i];
+      if (altura > altMaxSoldado) {
+        altMaxSoldado = altura;
+        altMaxSoldadoIndex = i;
+      }
+      if (altura <= altMinSoldado) {
+        altMinSoldado = altura;
+        altMinSoldadoIndex = i;
+      }
     }
-  );
+
+    contador = altMaxSoldadoIndex + (n - 1 - altMinSoldadoIndex);
+
+    if (altMaxSoldadoIndex > altMinSoldadoIndex) {
+      contador--;
+    }
+
+    console.log(contador);
+    rl.close();
+  }
+});
